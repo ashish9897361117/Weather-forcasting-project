44461f9972a00f92a9126923832ba82f97b0bd8f
ashish9897361117 <ashishkush9758581707@gmail.com>
2026-03-29 10:57:37 +0530

    Add files via upload

---
+++ model_training.py
@@ -0,0 +1,43 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import joblib
+import pandas as pd
+# Load the processed dataset
+df = pd.read_csv(r"C:\Users\Ashish Kushwah\OneDrive\Weather Forcasting\data\data\processed_weather_eda.csv")
+df.head()
+
+
+# Define features and target variable
+X = df.drop("RainTomorrow", axis=1)
+y = df["RainTomorrow"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+from sklearn.ensemble import RandomForestClassifier
+
+model = RandomForestClassifier(
+    class_weight='balanced',
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
+print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
+df[df["RainTomorrow"] == 1].sample(1)
+
+import joblib
+
+joblib.dump(model, "weather_model.pkl")
+
+import joblib
+
+joblib.dump(X.columns, "model_features.pkl")
